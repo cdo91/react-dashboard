6466c5692f2cf37e6dcb299dac2af9439ffa2070
Christian Olofsson <ch.olofsson@protonmail.com>
2024-05-17 23:45:28 +0200

    Added routes and the new dashboard component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { useState } from "react";
-import Topbar from "./scenes/global/Topbar";
-import Sidebar from "./scenes/global/Sidebar";
+import { Routes, Route } from "react-router-dom";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
+import Topbar from "./scenes/global/Topbar";
+import Sidebar from "./scenes/global/Sidebar";
+import Dashboard from "./scenes/dashboard/Dashboard";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -16,6 +18,9 @@ function App() {
           <Sidebar isSidebar={isSidebar} />
           <main className="content">
             <Topbar setIsSidebar={setIsSidebar} />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+            </Routes>
           </main>
         </div>
       </ThemeProvider>
